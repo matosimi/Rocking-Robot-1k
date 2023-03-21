9b0d70600322f1dcb900bc6bc8492de8fbd81487
Martin Simecek <martin.simecek@dxc.com>
2023-03-21 09:42:56 +0100

    removed commented out blocks for better readability

--- rocking_robot.asm
+++ rocking_robot.asm
@@ -1,7 +1,6 @@
 
 	org $80
 position	.ds 1
-;page	.ds 1
 	run code
 	org $2000
 song_data
@@ -157,7 +156,7 @@ song_end
 
 gfx	ins "krp5b.dat"
 :4	dta 0
-	;org $2000
+
 code	mwa #dl $230
 	mva #$80 $26f 
 	mva #32+1 559
@@ -168,66 +167,13 @@ code	mwa #dl $230
 	sta $2c0,x
 	dex 
 	bpl @-
-	;unrolled color loop - did not help to compress better
-/*	ldx #0
-	mva #$0e $2c0,x
-	inx
-	mva #$0c $2c0,x
-	inx
-	mva #$0a $2c0,x
-	inx
-	mva #$78 $2c0,x
-	inx
-	mva #$74 $2c0,x
-	inx
-	mva #$04 $2c0,x
-	inx
-	mva #$7a $2c0,x
-	inx
-	mva #$00 $2c0,x 
-	*/
-	;generated DL - faulty - but anyway did not save enough space
-/*	
-	lda #$70
-	sta dl
-	sta dl+1
-	sta dl+2
-	ldx #3
-dlloop	ldy #4
-@	mva #$5f dl,x
-	lda pool:#$00 ;<gfx
-	inx
-	sta dl,x
-	lda pooh:#$20 ;>gfx
-	inx
-	sta dl,x
-	inx
-	dey
-	bne @-
-	
-	lda pool
-	add #32
-	bcc @+
-	inc pooh
-@	sta pool
-	
-	dec cnt
-	bne dlloop
-	
-	mva #$41 dl,x
-	inx
-	mwa #dl dl,x*/
 
 	jmp music_start
 cnt	dta 38	
 
 ud	dta $10,$20,$40,$60,$70,$70,$80,$80
 	dta $80,$80,$70,$70,$50,$30,$10,$10
-
-	;dta $70,$60,$60,$50,$50,$50,
 clrs	dta $0e,$0c,$0a,$78,$74,$04,$7a,$00
-
-;dl	equ $8000
 
 dl	dta $70,$70,$70
 .rept 38,#
@@ -237,10 +183,8 @@ dl	dta $70,$70,$70
 	dta $41,a(dl)
 
 ;music player
-
 music_start
 	mva #0 position
-	;sta page
 	sta $4d	;attract
 loop	lda:cmp:req 20
 	and #$1f
@@ -252,8 +196,6 @@ loop	lda:cmp:req 20
 	and #$fe
 	sta $d404 ;hscrol
 	
-	;lda page
-	;bne @+
 	ldx position
 :9	mva song_data+192*#,x $d200+#  ;due to some small bug #193 required for binary file mlat.9st
 	inc position
@@ -261,19 +203,4 @@ loop	lda:cmp:req 20
 	cmp #192
 	bne loop
 	beq music_start
-	/*
-	bne loop
-	inc page
-	bne loop ;jmp loop
-	;----
-		
-@	ldx position
-:9	mva song_data+384*#+256,x $d200+#
-	inc position
-	lda position
-	cmp #384-256
-	jeq music_start
-	jmp loop
-
-*/
 
